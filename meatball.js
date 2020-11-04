300b67e264b245a70461cf5ea34025a2c14a63c6
CleverlyDone <pierregober@gmail.com>
2020-11-04 10:54:35 -0800

    20201104 - tested out some older code

--- meatball.js
+++ meatball.js
@@ -7,16 +7,22 @@
   window.addEventListener("hashchange", function () {
     getListItems();
   });
-
+  function buildScript(source) {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.src = source;
+    document.body.appendChild(script);
+    return script;
+  }
   /* get all the choices and send to main func*/
   function getListItems() {
-    var scriptAjax = document.createElement("script");
-    scriptAjax.type = "text/javascript";
-    scriptAjax.src =
-      "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js";
-    document.body.appendChild(scriptAjax);
-    //Waits till Ajax loads to allow full functionality of
-    scriptAjax.onload = function () {
+    var scripts = [
+      "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js",
+      "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js",
+    ].map(buildScript);
+    console.log(scripts);
+    scripts[0].onload = function () {
+      //Waits till Ajax loads to allow full functionality of
       //Step 1. Get all the tables -- create array
       var tables = [].slice.call(document.getElementsByTagName("table"));
       if (errorChecking(tables)) {
@@ -29,6 +35,8 @@
       });
       //Grabbing the list url
 
+      var axios = require;
+
       //Iterate through the table
       tables.forEach(function (table, index) {
         var currentListName = table.getAttribute("id").substring(1, 37);
@@ -38,7 +46,6 @@
           __metadata: { type: listName },
         };
         var url = root + "/_api/web/lists('" + currentListName + "')/fields";
-
         $.ajax({
           url: url,
           type: "GET",
@@ -69,7 +76,9 @@
                       if (column.indexOf("[") > -1) {
                         column = column.substring(1, column.length - 1);
                       }
-                      acc.externalColumn.push(column);
+                      if (acc.externalColumn.indexOf(column) < 0) {
+                        acc.externalColumn.push(column);
+                      }
                     }
                   }
                   return acc;
@@ -80,7 +89,7 @@
                   value: [],
                 }
               );
-
+              console.log("popoverData:", popoverData);
               popoverData.value.forEach(function (item, i) {
                 findTargets(
                   table,
@@ -100,76 +109,118 @@
     };
   }
 
+  var styler = function ({ color, width }) {
+    return `width:${width}px;height:${width}px;border-radius:${
+      width / 2
+    }px;background-color:${color}`;
+  };
+
   //Entry Point and General Function
-  function findTargets(table, values, externalColumn, internalColumn) {
-    if (!table || table.childNodes.length === 0) {
+  function findTargets($table, values, externalColumn, internalColumn, colors) {
+    if (!$table || $table.childNodes.length === 0) {
       return;
     }
 
+    //deafult keywords that have values.
+    var defaults = [
+      { color: "green", text: "up" },
+      { color: "red", text: "down" },
+      { color: "yellow", text: "degraded" },
+      { color: "green", text: "100-90" },
+      { color: "yellow", text: "89-80" },
+      { color: "red", text: "79-10" },
+      { color: "steelblue", text: "<10" },
+    ];
+
     //Step 3. Iterate over each cell and compare the inner text to the list of known defaults.
-    var rows = [].slice.call(table.getElementsByTagName("tr"));
-    var thead = [].slice.call(table.getElementsByTagName("th"));
+    var $rows = [].slice.call($table.getElementsByTagName("tr"));
+    var thead = [].slice.call($table.getElementsByTagName("th"));
     var displayValue = "";
     var displayColor = "";
 
-    rows.map(function (row, ri) {
+    $rows.map(function ($row, ri) {
       displayValue = "";
-      var cells = [].slice.call(row.getElementsByTagName("td"));
+      var $cells = [].slice.call($row.getElementsByTagName("td"));
 
-      if (cells.length > 0) {
+      if ($cells.length > 0) {
         //this checks if the cell contains the text which is in user choices, select that cell to add the modal
-        cells.map(function (cell, ci) {
-          var add = false;
-          if (thead[ci]) {
-            [].slice.call(thead[ci].children).forEach(function (item, ti) {
-              [].slice.call(item.children).forEach(function (item, tci) {
-                if (item.innerText) {
-                  if (containsString(item.innerText, "status")) {
-                    add =
-                      !containsString(item.innerText, "value") &&
-                      !containsString(item.innerText, "type");
-                    if (containsString(externalColumn, item.innerText)) {
-                      //Pseudo function
-                      if (
-                        externalColumn.split(" ").length - 1 ===
-                        item.innerText.split(" ").length
-                      ) {
-                        displayValue = cell.innerText;
-                      }
-                    } else {
-                      add = false;
-                    }
-                  }
-                }
-              });
-            });
+        $cells.map(function ($cell, ci) {
+          //new logic
+          var text = $cell.innerText;
+          var defaultText = defaults.map(function (a) {
+            return a.text;
+          });
+          var pos = defaultText.indexOf(text.toLowerCase());
+
+          //get the mouse Event over the cell
+          // $(cell).hover(function () {
+          //   var position = $(this).offset();
+          // });
+          console.log("pos:", pos, "\ncell:", $cell);
+
+          if (pos > -1) {
+            $cell.innerHTML = `<div data-toggle='tooltip' title="moreInformation" ><div style=${styler(
+              { color: defaults[pos].color, width: 20 }
+            )}></div></div>`;
           }
 
-          if (add && table.getAttribute("id") && row.getAttribute("iid")) {
-            [].slice.call(cell.children).forEach(function (item, i) {
-              [].slice.call(item.children).forEach(function (item, i) {
-                if (!displayValue) {
-                  displayValue = item.getAttribute("key");
-                } else if (displayValue.length < 1) {
-                  displayValue = item.getAttribute("key");
-                }
-              });
-            });
-            if (displayValue) {
-              addPopover(
-                cell,
-                values,
-                displayValue,
-                row.getAttribute("iid").split(",")[1],
-                thead[ci],
-                table.getAttribute("id").substring(1, 37),
-                externalColumn,
-                internalColumn
-              );
-            }
-          }
+          //Get the position of the td and find the c
+          //old logic of finding the cell and assinging values
+          // var add = false;
+          // if (thead[ci]) {
+          //   [].slice.call(thead[ci].children).forEach(function (item, ti) {
+          //     [].slice.call(item.children).forEach(function (item, tci) {
+          //       if (item.innerText) {
+          //         if (containsString(item.innerText, "status")) {
+          //           add =
+          //             !containsString(item.innerText, "value") &&
+          //             !containsString(item.innerText, "type");
+          //           if (containsString(externalColumn, item.innerText)) {
+          //             //Pseudo function
+          //             if (
+          //               externalColumn.split(" ").length - 1 ===
+          //               item.innerText.split(" ").length
+          //             ) {
+          //               displayValue = cell.innerText;
+          //             }
+          //           } else {
+          //             add = false;
+          //           }
+          //         }
+          //       }
+          //     });
+          //   });
+          // }
+          //
+          // if (add && table.getAttribute("id") && row.getAttribute("iid")) {
+          //   [].slice.call(cell.children).forEach(function (item, i) {
+          //     [].slice.call(item.children).forEach(function (item, i) {
+          //       if (!displayValue) {
+          //         displayValue = item.getAttribute("key");
+          //       } else if (displayValue.length < 1) {
+          //         displayValue = item.getAttribute("key");
+          //       }
+          //     });
+          //   });
+          //   if (displayValue) {
+          //     addPopover(
+          //       cell,
+          //       values,
+          //       displayValue,
+          //       row.getAttribute("iid").split(",")[1],
+          //       thead[ci],
+          //       table.getAttribute("id").substring(1, 37),
+          //       externalColumn,
+          //       internalColumn
+          //     );
+          //   }
+          // }
         });
       }
+    });
+    $(`[data-toggle="tooltip"]`).popover({
+      placement: "right",
+      trigger: "hover",
     });
   }
 
@@ -188,12 +239,11 @@
   ) {
     //Create Popover Element
     var popover = document.createElement("div");
-    popover.style.backgroundColor = "	#676767";
-    popover.style.color = "#f2f3f4";
+    popover.style.backgroundColor = "	#ffffff";
+    popover.style.color = "#000000";
     popover.style.padding = ".5rem";
     popover.style.border = "1px solid";
     popover.style.borderRadius = ".25rem";
-    popover.style.fontWeight = "bold";
     popover.style.zIndex = "1";
 
     //Create Header Element
@@ -203,15 +253,12 @@
     header.style.textAlign = "center";
     header.style.cursor = "pointer";
     header.style.marginBottom = ".25rem";
-    header.style.backgroundColor = "#3D71EB";
+    header.style.backgroundColor = "#BABBFD";
     header.innerText = displayValue;
-    header.style.textShadow = "1px 1px 1px black";
 
     //Create Options Panel Element
     var options = document.createElement("div");
-    options.style.display = "none";
     options.style.padding = ".25rem";
-    // options.style.backgroundColor = "#1C9BF0";
     options.style.borderRadius = ".25rem";
 
     //Create and Add Option Elements
@@ -220,7 +267,6 @@
       optionPanel.style.padding = ".25rem";
       optionPanel.style.marginBottom = ".25rem";
       optionPanel.style.textAlign = "left";
-      optionPanel.style.fontWeight = "bold";
       optionPanel.style.borderRadius = ".25rem";
 
       var option = document.createElement("div");
@@ -232,17 +278,15 @@
       radio.style.margin = "0px";
       radio.style.display = "inline";
 
-      option.style.textShadow = "1px 1px 1px black";
-      radio.style.color = "#f5f5f5";
-      option.style.color = "#f5f5f5";
-      optionPanel.style.backgroundColor = "#3D71EB";
-
       if (containsString(ele, displayValue)) {
         radio.checked = "checked";
+        optionPanel.style.backgroundColor = "#BABBFD";
       } else {
         radio.style.cursor = "pointer";
         optionPanel.style.cursor = "pointer";
         optionPanel.addEventListener("click", function () {
+          radio.checked = "checked";
+          optionPanel.style.backgroundColor = "#BABBFD";
           updateTarget(
             ele,
             rowIndex,
@@ -252,7 +296,14 @@
             internalColumn
           );
         });
+        optionPanel.addEventListener("mouseenter", function () {
+          optionPanel.style.boxShadow = "0px 0px 10px #BABBFD";
+        });
+        optionPanel.addEventListener("mouseleave", function () {
+          optionPanel.style.boxShadow = "0px 0px 0px";
+        });
       }
+
       //Add Click Event to update list
       optionPanel.appendChild(radio);
       optionPanel.appendChild(option);
@@ -341,7 +392,7 @@
         "X-RequestDigest": $("#__REQUESTDIGEST").val(),
       },
       success: function (data) {
-        alert("Updated" + header + "Successfully");
+        alert("Updated " + header + " Successfully");
         location.reload();
         return false;
       },
