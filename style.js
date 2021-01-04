13dedf4a6760a0e6c28b6aad24d8f3c7bf89e1a3
mathgrad <jscrager@gmail.com>
2021-01-04 13:13:32 -0800

    Style.js created
    Initialized

---
+++ style.js
@@ -0,0 +1,88 @@
+(function () {
+  function style(props) {
+    this.$ele = "";
+    switch (props.type) {
+      case "avatar":
+        break;
+      case "button":
+        break;
+      case "carret":
+        break;
+      case "container":
+        break;
+      case "meatball":
+        break;
+      case "message":
+        break;
+      default:
+        break;
+    }
+    switch (props.size) {
+      case "small":
+        break;
+      case "normal":
+        break;
+      case "large":
+        break;
+      default:
+        break;
+    }
+    switch (props.color) {
+      case "0":
+        //#f0f0f0;
+        break;
+      case "1":
+        //#e53935;
+        break;
+      case "2":
+        //#e91e63;
+        break;
+      case "3":
+        //#8e24aa;
+        break;
+      case "4":
+        //#3949ab;
+        break;
+      case "5":
+        //#00796b;
+        break;
+      case "6":
+        //#64dd17;
+        break;
+      case "7":
+        //#f4511e;
+        break;
+      case "8":
+        //#aaaaaa;
+        break;
+      case "9":
+        //#f96816;
+        break;
+      case "10":
+        //#222;
+        break;
+      case "11":
+        //#191919;
+        break;
+      case "12":
+        //#2c2c2c;
+        break;
+      case "13":
+        //#dfdfdf;
+        break;
+      case "14":
+        //#1b2b8d;
+        break;
+      case "15":
+        //#333333
+        break;
+      case "16":
+        //#202020;
+        break;
+      default:
+        //#f0f0f0;
+        break;
+    }
+    return this;
+  }
+});
