7bf5559ea9e85bd0a5e872124b9d2a9005699a85
mathgrad <jscrager@gmail.com>
2021-01-04 14:57:05 -0800

    Avatar
    Added in Avatar section

--- style.js
+++ style.js
@@ -5,12 +5,28 @@
       case "avatar":
         switch (props.size) {
           case "small":
+            this.$ele +=
+              "width: 15px; height: 15px; font-size: 7px; border-radius: 15px; text-align: center; line-height: 14px; background-color: " +
+              color(props.bgc) +
+              ";";
             break;
           case "normal":
+            this.$ele +=
+              "width: 30px; height: 30px; font-size: 14px; border-radius: 30px; text-align: center; line-height: 28px; background-color: " +
+              color(props.bgc) +
+              ";";
             break;
           case "large":
+            this.$ele +=
+              "width: 45px; height: 45px; font-size: 21px; border-radius: 45px; text-align: center; line-height: 42px; background-color: " +
+              color(props.bgc) +
+              ";";
             break;
           default:
+            this.$ele +=
+              "width: 30px; height: 30px; font-size: 14px; border-radius: 30px; text-align: center; line-height: 28px; background-color: " +
+              color(props.bgc) +
+              ";";
             break;
         }
         return this;
