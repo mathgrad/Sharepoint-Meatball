4d08f3c3003292a1ef9fe305d175289f652d662f
mathgrad <jscrager@gmail.com>
2021-01-05 09:20:29 -0800

    Update style.js
    Cleaned up color and background-color to be a single call at the bottom of the function.

--- style.js
+++ style.js
@@ -1,4 +1,5 @@
 (function () {
+  var color = new Color();
   /*
     props properties:
     type
@@ -13,27 +14,19 @@
         switch (props.size) {
           case "small":
             this.$ele +=
-              "width: 15px; height: 15px; font-size: 5.25pt; border-radius: 15px; text-align: center; line-height: 14px; background-color: " +
-              color(props.bgc) +
-              ";";
+              "width: 15px; height: 15px; font-size: 5.25pt; border-radius: 15px; text-align: center; line-height: 14px;";
             break;
           case "normal":
             this.$ele +=
-              "width: 30px; height: 30px; font-size: 10.5pt; border-radius: 30px; text-align: center; line-height: 28px; background-color: " +
-              color(props.bgc) +
-              ";";
+              "width: 30px; height: 30px; font-size: 10.5pt; border-radius: 30px; text-align: center; line-height: 28px;";
             break;
           case "large":
             this.$ele +=
-              "width: 45px; height: 45px; font-size: 15.75pt; border-radius: 45px; text-align: center; line-height: 42px; background-color: " +
-              color(props.bgc) +
-              ";";
+              "width: 45px; height: 45px; font-size: 15.75pt; border-radius: 45px; text-align: center; line-height: 42px;";
             break;
           default:
             this.$ele +=
-              "width: 30px; height: 30px; font-size: 10.5pt; border-radius: 30px; text-align: center; line-height: 28px; background-color: " +
-              color(props.bgc) +
-              ";";
+              "width: 30px; height: 30px; font-size: 10.5pt; border-radius: 30px; text-align: center; line-height: 28px;";
             break;
         }
         return this;
@@ -41,35 +34,19 @@
         switch (props.size) {
           case "small":
             this.$ele +=
-              "border-radius: .125rem; padding: .125rem; margin: 0px .25rem .25rem .25rem; text-align: center; display: block; cursor: pointer: font-weight: 500; background-color: " +
-              color(props.bgc) +
-              "; color: " +
-              color(props.fc) +
-              ";";
+              "border-radius: .125rem; padding: .125rem; margin: 0px .25rem .25rem .25rem; text-align: center; display: block; cursor: pointer: font-weight: 500;";
             break;
           case "normal":
             this.$ele +=
-              "border-radius: .25rem; padding: .25rem; margin: 0px .5rem .5rem .5rem; text-align: center; display: block; cursor: pointer: font-weight: 500; background-color: " +
-              color(props.bgc) +
-              "; color: " +
-              color(props.fc) +
-              ";";
+              "border-radius: .25rem; padding: .25rem; margin: 0px .5rem .5rem .5rem; text-align: center; display: block; cursor: pointer: font-weight: 500;";
             break;
           case "large":
             this.$ele +=
-              "border-radius: .5rem; padding: .5rem; margin: 0px 1rem 1rem 1rem; text-align: center; display: block; cursor: pointer: font-weight: 500; background-color: " +
-              color(props.bgc) +
-              "; color: " +
-              color(props.fc) +
-              ";";
+              "border-radius: .5rem; padding: .5rem; margin: 0px 1rem 1rem 1rem; text-align: center; display: block; cursor: pointer: font-weight: 500;";
             break;
           default:
             this.$ele +=
-              "border-radius: .25rem; padding: .25rem; margin: 0px .5rem .5rem .5rem; text-align: center; display: block; cursor: pointer: font-weight: 500; background-color: " +
-              color(props.bgc) +
-              "; color: " +
-              color(props.fc) +
-              ";";
+              "border-radius: .25rem; padding: .25rem; margin: 0px .5rem .5rem .5rem; text-align: center; display: block; cursor: pointer: font-weight: 500;";
             break;
         }
         return this;
@@ -78,25 +55,25 @@
           case "small":
             this.$ele +=
               "margin: 0px; display: inline-block; position:absolute; height: 0px; width: 0px; left: 2px; top: 14px; border-top: 5px solid transparent; border-bottom: 5px solid transparent; border-right: 5px solid " +
-              color(props.bgc) +
+              color.get(props.bgc) +
               ";";
             break;
           case "normal":
             this.$ele +=
               "margin: 0px; display: inline-block; position:absolute; height: 0px; width: 0px; left: 2px; top: 29px; border-top: 10px solid transparent; border-bottom: 10px solid transparent; border-right: 10px solid " +
-              color(props.bgc) +
+              color.get(props.bgc) +
               ";";
             break;
           case "large":
             this.$ele +=
               "margin: 0px; display: inline-block; position:absolute; height: 0px; width: 0px; left: 2px; top: 44px; border-top: 15px solid transparent; border-bottom: 15px solid transparent; border-right: 15px solid " +
-              color(props.bgc) +
+              color.get(props.bgc) +
               ";";
             break;
           default:
             this.$ele +=
               "margin: 0px; display: inline-block; position:absolute; height: 0px; width: 0px; left: 2px; top: 29px; border-top: 10px solid transparent; border-bottom: 10px solid transparent; border-right: 10px solid " +
-              color(props.bgc) +
+              color.get(props.bgc) +
               ";";
             break;
         }
@@ -117,27 +94,19 @@
         switch (props.size) {
           case "small":
             this.$ele +=
-              "width: 10px; height: 10px; border-radius: 10px; margin: auto; padding: 0px; background-color: " +
-              color(props.bgc) +
-              ";";
+              "width: 10px; height: 10px; border-radius: 10px; margin: auto; padding: 0px;";
             break;
           case "normal":
             this.$ele +=
-              "width: 15px; height: 15px; border-radius: 15px; margin: auto; padding: 0px; background-color: " +
-              color(props.bgc) +
-              ";";
+              "width: 15px; height: 15px; border-radius: 15px; margin: auto; padding: 0px;";
             break;
           case "large":
             this.$ele +=
-              "width: 20px; height: 20px; border-radius: 20px; margin: auto; padding: 0px; background-color: " +
-              color(props.bgc) +
-              ";";
+              "width: 20px; height: 20px; border-radius: 20px; margin: auto; padding: 0px;";
             break;
           default:
             this.$ele +=
-              "width: 15px; height: 15px; border-radius: 15px; margin: auto; padding: 0px; background-color: " +
-              color(props.bgc) +
-              ";";
+              "width: 15px; height: 15px; border-radius: 15px; margin: auto; padding: 0px;";
             break;
         }
         return this;
@@ -157,36 +126,27 @@
         switch (props.size) {
           case "small":
             this.$ele +=
-              "display: inline-block; margin: 0px; padding: 0px; width: 100px; box-shadow: 1px 1px 4px 1px rgb(0 0 0 / 0.2); color: " +
-              color(props.fc) +
-              "; background-color: " +
-              color(props.bgc) +
-              ";";
+              "display: inline-block; margin: 0px; padding: 0px; width: 100px; box-shadow: 1px 1px 4px 1px rgb(0 0 0 / 0.2);";
             break;
           case "normal":
             this.$ele +=
-              "display: inline-block; margin: 0px; padding: 0px; width: 200px; box-shadow: 1px 1px 4px 1px rgb(0 0 0 / 0.2); color: " +
-              color(props.fc) +
-              "; background-color: " +
-              color(props.bgc) +
-              ";";
+              "display: inline-block; margin: 0px; padding: 0px; width: 200px; box-shadow: 1px 1px 4px 1px rgb(0 0 0 / 0.2);";
             break;
           case "large":
             this.$ele +=
-              "display: inline-block; margin: 0px; padding: 0px; width: 300px; box-shadow: 1px 1px 4px 1px rgb(0 0 0 / 0.2); color: " +
-              color(props.fc) +
-              "; background-color: " +
-              color(props.bgc) +
-              ";";
+              "display: inline-block; margin: 0px; padding: 0px; width: 300px; box-shadow: 1px 1px 4px 1px rgb(0 0 0 / 0.2);";
             break;
           default:
             this.$ele +=
-              "display: inline-block; margin: 0px; padding: 0px; width: 200px; box-shadow: 1px 1px 4px 1px rgb(0 0 0 / 0.2); color: " +
-              color(props.fc) +
-              "; background-color: " +
-              color(props.bgc) +
-              ";";
+              "display: inline-block; margin: 0px; padding: 0px; width: 200px; box-shadow: 1px 1px 4px 1px rgb(0 0 0 / 0.2);";
             break;
+        }
+
+        if (props.fc) {
+          this.$ele += "color: " + color.get(props.fc) + ";";
+        }
+        if (props.bgc) {
+          this.$ele += "background-color: " + color.get(props.bgc) + ";";
         }
         return this;
       }
