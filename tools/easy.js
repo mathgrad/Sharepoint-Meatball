b262ea78e964f825eb0e9b3b6b07c60488a2cb88
mathgrad <jscrager@gmail.com>
2021-03-23 09:40:53 -0700

    Update easy.js
    Removed the window on load

--- tools/easy.js
+++ tools/easy.js
@@ -5,144 +5,142 @@ function easyStart() {
     ".MenuButton{background-color: #1890ff; border: 0px; border-radius: .25rem; color: #ddd; cursor: pointer; font-weight: bold; padding: .25rem;}" +
     ".MenuButton:hover{background-color: #ddd; color: #1890ff}";
   document.getElementsByTagName("head")[0].appendChild(styleSheet);
-  window.addEventListener("load", function () {
-    //Styles
-    var meatballCustomizationMenuStyle =
-      "align-items: center; background-color: #ddd; border: .25rem solid #000; border-radius: 1.5rem; bottom: 104px; color: #222; display: flex; flex-direction: column; height: 200px; justify-content: space-between: overflow: hidden auto; padding: .25rem; position: fixed; right: calc(155px + 5.5vw); width: 150px;";
+  //Styles
+  var meatballCustomizationMenuStyle =
+    "align-items: center; background-color: #ddd; border: .25rem solid #000; border-radius: 1.5rem; bottom: 104px; color: #222; display: flex; flex-direction: column; height: 200px; justify-content: space-between: overflow: hidden auto; padding: .25rem; position: fixed; right: calc(155px + 5.5vw); width: 150px;";
 
-    var cv = {
-      main:
-        "align-item: center; display: flex; flex-direction: row; flex-wrap: wrap; justify-content: space-between;",
-      child:
-        "display: flex; flex-shrink: 1; flex-grow: 2; line-height: 1.5rem; padding: .25rem;",
-      circle:
-        "border-radius: 100%; height: 15px; margin: .25rem auto; padding: .25rem; width: 15px;",
+  var cv = {
+    main:
+      "align-item: center; display: flex; flex-direction: row; flex-wrap: wrap; justify-content: space-between;",
+    child:
+      "display: flex; flex-shrink: 1; flex-grow: 2; line-height: 1.5rem; padding: .25rem;",
+    circle:
+      "border-radius: 100%; height: 15px; margin: .25rem auto; padding: .25rem; width: 15px;",
+  };
+
+  var start = new Menu();
+  var meatballItem = new MenuItem("Meatball");
+  var modal = new Modal();
+  var meatballSubMenu = {};
+  meatballSubMenu.title = "Meatball";
+  meatballSubMenu.bottom = "calc(80px + 1.5rem)";
+  meatballSubMenu.right = "calc(200px + 2vw)";
+  meatballSubMenu.title = "Customize";
+  meatballSubMenu.menuItems = [];
+
+  //Creates the Color Value Menu Item
+  function CVM() {
+    this.cvMI = new MenuItem("Color and Value");
+    this.cvMIContent = {
+      title: "Color and Value Customization",
+      body: [],
+      footer: [],
     };
 
-    var start = new Menu();
-    var meatballItem = new MenuItem("Meatball");
-    var modal = new Modal();
-    var meatballSubMenu = {};
-    meatballSubMenu.title = "Meatball";
-    meatballSubMenu.bottom = "calc(80px + 1.5rem)";
-    meatballSubMenu.right = "calc(200px + 2vw)";
-    meatballSubMenu.title = "Customize";
-    meatballSubMenu.menuItems = [];
+    this.cvMIBody = new MeatballMenuColorSelector({
+      columns: [],
+    });
+    var mmcs = this.cvMIBody;
 
-    //Creates the Color Value Menu Item
-    function CVM() {
-      this.cvMI = new MenuItem("Color and Value");
-      this.cvMIContent = {
-        title: "Color and Value Customization",
-        body: [],
-        footer: [],
-      };
+    this.cvMIContent.body.push(this.cvMIBody.$ele);
+    this.cvSubmit = document.createElement("button");
+    this.cvSubmit.innerText = "Submit";
+    this.cvSubmit.className += "MenuButton";
+    this.cvSubmit.addEventListener("click", function (e) {
+      e.stopPropagation();
+      mmcs.updateCurrent();
+      console.log(mmcs.getValues());
+      this.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild(
+        this.parentNode.parentNode.parentNode.parentNode.parentNode
+      );
+    });
 
-      this.cvMIBody = new MeatballMenuColorSelector({
-        columns: [],
-      });
-      var mmcs = this.cvMIBody;
+    this.cvMIContent.footer.push(this.cvSubmit);
 
-      this.cvMIContent.body.push(this.cvMIBody.$ele);
-      this.cvSubmit = document.createElement("button");
-      this.cvSubmit.innerText = "Submit";
-      this.cvSubmit.className += "MenuButton";
-      this.cvSubmit.addEventListener("click", function (e) {
-        e.stopPropagation();
-        mmcs.updateCurrent();
-        console.log(mmcs.getValues());
-        this.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild(
-          this.parentNode.parentNode.parentNode.parentNode.parentNode
-        );
-      });
+    this.cvMI.addModal({ modal: modal, content: this.cvMIContent });
 
-      this.cvMIContent.footer.push(this.cvSubmit);
+    this.cvMI.$ele.addEventListener("click", function () {
+      mmcs.updateChoices();
+    });
+    return this.cvMI.$ele;
+  }
 
-      this.cvMI.addModal({ modal: modal, content: this.cvMIContent });
+  meatballSubMenu.menuItems.push(new CVM());
 
-      this.cvMI.$ele.addEventListener("click", function () {
-        mmcs.updateChoices();
-      });
-      return this.cvMI.$ele;
-    }
+  function CTypeM() {
+    this.cvMI = new MenuItem("Meatball Type");
+    this.cvMIContent = {
+      title: "Set Type",
+      body: [],
+      footer: [],
+    };
 
-    meatballSubMenu.menuItems.push(new CVM());
+    this.cvMIBody = new MeatballMenuTypeContent();
+    var cvmibody = this.cvMIBody;
+    this.cvMIBody.setValues({
+      columns: [
+        {
+          name: "Text",
+          value: "circle",
+        },
+        { name: "Test", value: "ignore" },
+        { name: "Hi", value: "text" },
+      ],
+    });
+    this.cvMIContent.body.push(this.cvMIBody.$ele);
+    this.cvSubmit = document.createElement("button");
+    this.cvSubmit.innerText = "Submit";
+    this.cvSubmit.className += "MenuButton";
+    this.cvSubmit.addEventListener("click", function (e) {
+      e.stopPropagation();
+      this.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild(
+        this.parentNode.parentNode.parentNode.parentNode.parentNode
+      );
+    });
 
-    function CTypeM() {
-      this.cvMI = new MenuItem("Meatball Type");
-      this.cvMIContent = {
-        title: "Set Type",
-        body: [],
-        footer: [],
-      };
+    this.cvMIContent.footer.push(this.cvSubmit);
 
-      this.cvMIBody = new MeatballMenuTypeContent();
-      var cvmibody = this.cvMIBody;
-      this.cvMIBody.setValues({
-        columns: [
-          {
-            name: "Text",
-            value: "circle",
-          },
-          { name: "Test", value: "ignore" },
-          { name: "Hi", value: "text" },
-        ],
-      });
-      this.cvMIContent.body.push(this.cvMIBody.$ele);
-      this.cvSubmit = document.createElement("button");
-      this.cvSubmit.innerText = "Submit";
-      this.cvSubmit.className += "MenuButton";
-      this.cvSubmit.addEventListener("click", function (e) {
-        e.stopPropagation();
-        this.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild(
-          this.parentNode.parentNode.parentNode.parentNode.parentNode
-        );
-      });
+    this.cvMI.addModal({ modal: modal, content: this.cvMIContent });
 
-      this.cvMIContent.footer.push(this.cvSubmit);
+    this.cvMI.$ele.addEventListener("click", function () {
+      cvmibody.updateValues();
+    });
 
-      this.cvMI.addModal({ modal: modal, content: this.cvMIContent });
+    return this.cvMI.$ele;
+  }
 
-      this.cvMI.$ele.addEventListener("click", function () {
-        cvmibody.updateValues();
-      });
+  meatballSubMenu.menuItems.push(new CTypeM());
 
-      return this.cvMI.$ele;
-    }
+  function CTM() {
+    this.cvMI = new MenuItem("Toggle Meatball");
+    this.cvMIContent = {
+      title: "Toggle Meatball",
+      body: [],
+      footer: [],
+    };
 
-    meatballSubMenu.menuItems.push(new CTypeM());
+    this.cvMIBody = new MeatballMenuToggleContent(true);
+    this.cvMIContent.body.push(this.cvMIBody.$ele);
+    this.cvSubmit = document.createElement("button");
+    this.cvSubmit.innerText = "Submit";
+    this.cvSubmit.className += "MenuButton";
+    this.cvSubmit.addEventListener("click", function (e) {
+      e.stopPropagation();
+      this.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild(
+        this.parentNode.parentNode.parentNode.parentNode.parentNode
+      );
+    });
 
-    function CTM() {
-      this.cvMI = new MenuItem("Toggle Meatball");
-      this.cvMIContent = {
-        title: "Toggle Meatball",
-        body: [],
-        footer: [],
-      };
+    this.cvMIContent.footer.push(this.cvSubmit);
 
-      this.cvMIBody = new MeatballMenuToggleContent(true);
-      this.cvMIContent.body.push(this.cvMIBody.$ele);
-      this.cvSubmit = document.createElement("button");
-      this.cvSubmit.innerText = "Submit";
-      this.cvSubmit.className += "MenuButton";
-      this.cvSubmit.addEventListener("click", function (e) {
-        e.stopPropagation();
-        this.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode.removeChild(
-          this.parentNode.parentNode.parentNode.parentNode.parentNode
-        );
-      });
+    this.cvMI.addModal({ modal: modal, content: this.cvMIContent });
+    return this.cvMI.$ele;
+  }
 
-      this.cvMIContent.footer.push(this.cvSubmit);
+  meatballSubMenu.menuItems.push(new CTM());
 
-      this.cvMI.addModal({ modal: modal, content: this.cvMIContent });
-      return this.cvMI.$ele;
-    }
+  meatballItem.addSubMenu(meatballSubMenu);
 
-    meatballSubMenu.menuItems.push(new CTM());
-
-    meatballItem.addSubMenu(meatballSubMenu);
-
-    start.addMenuItem({ child: meatballItem, $ele: meatballItem.$ele });
-    start.init();
-  });
+  start.addMenuItem({ child: meatballItem, $ele: meatballItem.$ele });
+  start.init();
 }
